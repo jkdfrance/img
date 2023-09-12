e52afc9c6f282c4624b8890e5018c30570402ad4
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 14:52:29 +0200

    Create depannage.php

---
+++ template/depannage.php
@@ -0,0 +1,25 @@
+<section id="cars">
+      <h1 class="section-title">Nos services de dépannage</h1>
+    </section>
+     <div class="list_services">
+        <div class="service">
+        <i class="fa-solid fa-screwdriver-wrench"></i>
+        <h3>Depannage</h3>
+        <p>Tout ce qu'il faut savoir sur nos services</p>
+        <a href="service.php" class="read">Lire</a>
+        </div>
+        </div>
+    </section>
+
+<section class="infos">
+<img src="image/dot.jpg">
+  <p> 789 garages dans toute la France</p>
+</section>
+
+<img  class="bg-cover" width="325" height="325" src="https://www.top-garage.fr/app/uploads/Meilleur-Atelier-de-France-top-garage-1.png" class="attachment-large size-large" alt="Top Garage" loading="lazy" srcset="https://www.top-garage.fr/app/uploads/Meilleur-Atelier-de-France-top-garage-1.png 325w, https://www.top-garage.fr/app/uploads/Meilleur-Atelier-de-France-top-garage-1-90x90.png 90w, https://www.top-garage.fr/app/uploads/Meilleur-Atelier-de-France-top-garage-1-120x120.png 120w, https://www.top-garage.fr/app/uploads/Meilleur-Atelier-de-France-top-garage-1-90x90@2x.png 180w" sizes="(max-width: 325px) 100vw, 320px">                                            </div>
+
+ 
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.183450181977!2d2.29244067531289!3d48.87377927133421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fec746d386b%3A0x8710a9c7965c5fa9!2sL&#39;Arc%20de%20Triomphe%20de%20l&#39;Etoile!5e0!3m2!1sfr!2sfr!4v1689771550105!5m2!1sfr!2sfr" width="550" height="320" ></iframe>
+
+ 
