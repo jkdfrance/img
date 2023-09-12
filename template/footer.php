6163734f8471dab5d279249700575eda3dd054d9
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 15:15:21 +0200

    Create footer.php

---
+++ template/footer.php
@@ -0,0 +1,23 @@
+<?php echo '<link rel="stylesheet" href="style.css">'; ?>
+
+<!--footer--page--revision.php-->
+</main>
+
+<?php echo '<link rel="stylesheet" href="acceuil.css">'; ?>
+
+  </main>
+
+  
+
+  <div class="footer">
+<footer class="py-3 my-4">
+    <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
+  </footer>
+</div>
+
+ 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <script src="index1.js"></script>
+  </body>
+</html>
