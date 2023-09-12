c3ff679876ea17c9381534bbf604eb9ec395a359
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 15:15:49 +0200

    Create footer-2.php

---
+++ template/footer-2.php
@@ -0,0 +1,16 @@
+<?php echo '<link rel="stylesheet" href="style.css">'; ?>
+
+<!--footer--page--revision.php-->
+</main>
+
+<div class="wrapper">
+            <div class="row">
+                <div class="col-md-12"><p class="commitment">Nos engagements</p></div>
+                <img class="fast" src="icone/fast.png" alt="icone rapidité">
+                <div class="col-md-3">Rapidité<br>et qualité d'exécution</div>
+                <img class="euro" src="icone/euro.png" alt="icone euro">
+                <div class="col-md-3">Tarifs<br>préférentiels</div>
+                <img class="service" src="icone/services.png" alt="icone service">
+                <div class="col-md-3">Révisions constructeurs<br> certifiées</div>
+            </div>
+        </div>
