5c31afb2db345675b3e5032ffebe9b4bde07012e
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 15:14:46 +0200

    Create header-service.php

---
+++ template/header-service.php
@@ -0,0 +1,195 @@
+<?php echo '<link rel="stylesheet" href="style.css">'; 
+
+
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">';
+echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />';
+echo '<script src="stylesheet" href="https://kit.fontawesome.com/de8de52639.js"  crossorigin="anonymous" ></script>';
+?>
+
+
+<!--creation-page-service.php-->
+
+<?php $server_uri = ($_SERVER["REQUEST_URI"]); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-3dZ9wIrMMij8rOH7X3kLfXAzwtcHpuYpEgQg1OA4QAob1e81H8ntUQmQm3pBudqIoySO5j0tHN4ENzA6+n2r4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css" />
+
+    <title>DEVIS</title>
+    <script src="stylesheet" href="https://kit.fontawesome.com/de8de52639.js"  crossorigin="anonymous" ></script>
+  </head>
+  <body>
+
+
+<header>
+<div class="elementor-widget-wrap elementor-element-populated">
+		<div class="elementor-element elementor-element-776d2cb elementor-widget elementor-widget-image" data-id="776d2cb" data-element_type="widget" data-widget_type="image.default">
+			<div class="elementor-widget-container">
+		<img width="100%" height="350rem" src="https://assistance-depanneuse.fr/wp-content/uploads/2023/08/towing-truck-driver-staying-next-to-his-vehicle.jpg" class="attachment-full size-full wp-image-84" alt="Towing Truck Driver" decoding="async" srcset="https://assistance-depanneuse.fr/wp-content/uploads/2023/08/towing-truck-driver-staying-next-to-his-vehicle.jpg 2000w, https://assistance-depanneuse.fr/wp-content/uploads/2023/08/towing-truck-driver-staying-next-to-his-vehicle-300x200.jpg 300w, https://assistance-depanneuse.fr/wp-content/uploads/2023/08/towing-truck-driver-staying-next-to-his-vehicle-1024x682.jpg 1024w, https://assistance-depanneuse.fr/wp-content/uploads/2023/08/towing-truck-driver-staying-next-to-his-vehicle-768x512.jpg 768w, https://assistance-depanneuse.fr/wp-content/uploads/2023/08/towing-truck-driver-staying-next-to-his-vehicle-1536x1024.jpg 1536w" sizes="(max-width: 2000px) 100vw, 2000px">															
+	</div>
+</div>
+				
+<div class="elementor-element elementor-element-3069e3eb elementor-widget elementor-widget-jkit_heading" data-id="3069e3eb" data-element_type="widget" data-widget_type="jkit_heading.default">
+		<div class="elementor-widget-container">
+			<div class="jeg-elementor-kit jkit-heading  align-left align-tablet- align-mobile- jeg_module_395_1_64f9adde4a906">
+				<div class="heading-section-title  display-inline-block">
+					<h2 class="heading-title">Le Remorquage en Île-de-France: Votre Allié sur la Route</h2>
+			</div>
+		</div>		
+	</div>
+</div>
+	
+<div class="elementor-element elementor-element-5f969d99 elementor-widget elementor-widget-text-editor" data-id="5f969d99" data-element_type="widget" data-widget_type="text-editor.default">
+	<div class="elementor-widget-container">
+		<p>En Île-de-France , les routes peuvent être imprévisibles. Que vous soyez en plein cœur de Paris ou sur une route de campagne , une panne ou un accident peut survenir à tout moment. C’est là qu’intervient notre entreprise de remorquage, un acteur essentiel dans le paysage automobile local.</p><h4>1. Notre Service de Remorquage</h4><p>Nous offrons un service de remorquage complet et fiable, capable de gérer toutes sortes de situations. Notre équipe expérimentée est équipée de dépanneuses modernes et prête à intervenir 24/7, que ce soit pour une simple panne ou un accident majeur.</p><h4>2. Zones de Service</h4><ul><li><strong>Île-de-France</strong>: De Paris aux coins les plus reculés de la région, nous sommes là pour vous assister.</li><li><strong>Oise</strong>: Notre service s’étend également dans l’Oise, garantissant une assistance rapide et professionnelle.</li></ul><h4>3. Types de Remorquage</h4><ul><li><strong>Remorquage d’Urgence</strong>: Disponible à toute heure, pour les situations urgentes.</li><li><strong>Remorquage de Véhicules</strong>: Qu’il s’agisse d’une voiture, d’un camion, ou d’une moto, nous avons l’équipement nécessaire.</li><li><strong>Remorquage Longue Distance</strong>: Besoin de transporter un véhicule sur de longues distances ? Nous pouvons vous aider.</li></ul><h4>4. Sécurité et Qualité</h4><p>La sécurité est notre priorité. Nous suivons des protocoles stricts et utilisons un équipement de pointe pour garantir une manipulation soignée de votre véhicule.</p><h4>5. Choisissez le Meilleur en Local</h4><p>Notre réputation en Île-de-France et dans l’Oise n’est plus à faire. Avec des années d’expérience et des témoignages positifs, nous sommes le choix local de confiance pour vos besoins en remorquage.</p><h4>Remorquage auto Paris IDF Oise</h4><p>Le remorquage n’est pas simplement un service, c’est un gage de tranquillité. En Île-de-France et dans l’Oise, notre entreprise est là pour vous garantir une assistance professionnelle et réactive. Ne vous laissez pas surprendre par l’inattendu sur la route, confiez vos besoins en remorquage à des experts locaux et fiables.</p>						
+	</div>
+</div>
+
+<!--section--sidebar-->
+
+<section class="assist">
+	<h2>ASSISTANCE REMORQUAGE</h2><br>
+		<p class="item">Notre assistance 24/7 est toujours disponible pour répondre à vos besoins en dépannage et remorquage, 
+						quelle que soit l’heure ou le jour.</p><br>
+		<p id="item-2">APELLER MAINTENANT<br>06123458</p>
+	</div>
+</section>
+
+<!--------->
+				
+<div class="elementor-element elementor-element-7b18f816 elementor-view-stacked elementor-position-left elementor-vertical-align-middle elementor-shape-square elementor-mobile-position-left elementor-widget elementor-widget-icon-box" data-id="7b18f816" data-element_type="widget" data-widget_type="icon-box.default">
+	<div class="elementor-widget-container">
+		<link rel="stylesheet" href="https://assistance-depanneuse.fr/wp-content/plugins/elementor/assets/css/widget-icon-box.min.css">		
+			<div class="elementor-icon-box-wrapper">
+				<div class="elementor-icon-box-icon">
+				<span class="elementor-icon elementor-animation-">
+		<i aria-hidden="true" class="fas fa-cog"></i>				
+	</span>
+</div>
+
+<div class="elementor-icon-box-content">
+	<h3 class="elementor-icon-box-title">
+		<span>Assistance Batterie
+			</h3>
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="elementor-section elementor-inner-section elementor-element elementor-element-1bde9b5f elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="1bde9b5f" data-element_type="section">
+	<div class="elementor-container elementor-column-gap-no">
+		<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-3a7ddaed" data-id="3a7ddaed" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+				<div class="elementor-element elementor-element-7f7f0637 elementor-align-left elementor-icon-list--layout-traditional elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list" data-id="7f7f0637" data-element_type="widget" data-widget_type="icon-list.default">
+					<div class="elementor-widget-container">
+					<link rel="stylesheet" href="https://assistance-depanneuse.fr/wp-content/plugins/elementor/assets/css/widget-icon-list.min.css">		
+			
+			<ul class="elementor-icon-list-items">
+				<li class="elementor-icon-list-item">
+					<span class="elementor-icon-list-icon">
+					<i aria-hidden="true" class="fas fa-check"></i>						
+				</span>
+					<span class="elementor-icon-list-text">Remorquage d'urgence</span>
+				</li>
+								
+		<li class="elementor-icon-list-item">
+			<span class="elementor-icon-list-icon">
+			<i aria-hidden="true" class="fas fa-check"></i>						
+			</span>
+			<span class="elementor-icon-list-text">Remorquage de véhicules légers et lourds</span>
+		</li>
+								
+		<li class="elementor-icon-list-item">
+			<span class="elementor-icon-list-icon">
+			<i aria-hidden="true" class="fas fa-check"></i>						
+			</span>
+			<span class="elementor-icon-list-text">Remorquage longue distance</span>
+		</li>
+		</ul>
+		</div>
+	</div>
+</div>
+</div>
+			
+<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-d82d57f" data-id="d82d57f" data-element_type="column">
+	<div class="elementor-widget-wrap elementor-element-populated">
+		<div class="elementor-element elementor-element-6354cdb5 elementor-align-left elementor-icon-list--layout-traditional elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list" data-id="6354cdb5" data-element_type="widget" data-widget_type="icon-list.default">
+			<div class="elementor-widget-container">
+				<ul class="elementor-icon-list-items">
+					<li class="elementor-icon-list-item">
+						<span class="elementor-icon-list-icon">
+							<i aria-hidden="true" class="fas fa-check"></i>						
+						</span>
+						<span class="elementor-icon-list-text">Remorquage après accident</span>
+					</li>
+								
+					<li class="elementor-icon-list-item">
+						<span class="elementor-icon-list-icon">
+							<i aria-hidden="true" class="fas fa-check"></i>						
+						</span>
+						<span class="elementor-icon-list-text">Service de remorquage 24/7</span>
+					</li>
+						<li class="elementor-icon-list-item">
+							<span class="elementor-icon-list-icon">
+							<i aria-hidden="true" class="fas fa-check"></i>						
+						</span>
+						<span class="elementor-icon-list-text">Entreprise de remorquage locale</span>
+					</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+<section class="elementor-section elementor-inner-section elementor-element elementor-element-7e3548a0 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="7e3548a0" data-element_type="section">
+	<div class="elementor-container elementor-column-gap-no">
+		<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-cd8ffed animated-slow animated fadeInUp" data-id="cd8ffed" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:20}">
+			<div class="elementor-widget-wrap elementor-element-populated">
+				<div class="elementor-element elementor-element-22e2df2e jkit-equal-height-disable elementor-widget elementor-widget-jkit_icon_box" data-id="22e2df2e" data-element_type="widget" data-widget_type="jkit_icon_box.default">
+					<div class="elementor-widget-container">
+						<div class="jeg-elementor-kit jkit-icon-box icon-position- elementor-animation- jeg_module_395_2_64f9adde5321c">
+							<div class="jkit-icon-box-wrapper hover-from-left"><div class="icon-box icon-box-header elementor-animation-">
+								<div class="icon style-color"><i aria-hidden="true" class="jki jki-cogwheel-light"></i>
+							</div>
+						</div>
+
+						<div class="icon-box icon-box-body">
+                		<h3 class="title">Remorquage</h3>
+						<p class="icon-box-description">Offrant une assistance professionnelle et réactive en Île-de-France et dans l'Oise, notre entreprise de remorquage est là pour vous garantir une tranquillité d'esprit sur la route, avec un service complet disponible 24/7.</p>
+            			</div>
+        			</div>
+				</div>		
+			</div>
+		</div>
+	</div>
+</div>
+				
+<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-5cb71b68 animated-slow animated fadeInUp" data-id="5cb71b68" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:40}">
+				<div class="elementor-widget-wrap elementor-element-populated">
+					<div class="elementor-element elementor-element-2b9b16ed jkit-equal-height-disable elementor-widget elementor-widget-jkit_icon_box" data-id="2b9b16ed" data-element_type="widget" data-widget_type="jkit_icon_box.default">
+						<div class="elementor-widget-container">
+							<div class="jeg-elementor-kit jkit-icon-box icon-position- elementor-animation- jeg_module_395_3_64f9adde56599"><div class="jkit-icon-box-wrapper hover-from-left"><div class="icon-box icon-box-header elementor-animation-"><div class="icon style-color"><i aria-hidden="true" class="jki jki-Mechanic-light"></i></div></div><div class="icon-box icon-box-body">
+                				<h3 class="title">Dépanneur Auto</h3><p class="icon-box-description">Nos dépanneurs spécialisés interviennent rapidement et vous remorquent à notre atelier pour une vidange complète.</p>
+            				</div>
+            			</div>
+					</div>		
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+					
+</main>
