1ee57f0a4b86f924b4308645f950b06fcd87efb1
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 14:51:33 +0200

    Create article-part.php

---
+++ template/article-part.php
@@ -0,0 +1,65 @@
+<?php echo '<link rel="stylesheet" href="acceuil.css">';
+      
+      $server_uri = ($_SERVER["REQUEST_URI"]); 
+?>
+
+<section>
+    <div class="slider">
+        <div class="slider-viewport">
+        <div id="img1">
+            <div id="img2">
+            <div id="img3">
+                <div id="img4">
+                <div id="img5">
+                    <div class="slider-content">
+                        <img src="image/franchises.webp">
+                        <img src="image/bugatti.jpg">
+                        <img src="image/pare-brise.jpg">
+                        <img src="image/promo.jpg">
+                        <img src="image/revision.jpg">
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="slider-nav">
+        <a href="#img1"></a>
+        <a href="#img2"></a>
+        <a href="#img3"></a>
+        <a href="#img4"></a>
+        <a href="#img5"></a>
+        </div>
+        </div>
+     </section>
+
+    <section>
+        <div class="element">
+          <p>Nos fournisseurs</p>
+        </div>  
+    </section>
+
+        <section class="container-logo ">
+        <div class="slide-img">
+    <div>
+      <img class="ferrari" src="logo /ferrari.jpg" alt="logo ferrari" width="100" >
+    </div>
+    <div>
+      <img src="logo/bmw.jpg" alt="logo bmw" width="100">
+    </div>
+    <div>
+      <img class="jaguar" src="logo /jaguar.jpg" alt="logo jaguar" width="150">
+    </div>
+    <div>
+      <img class="maserati" src="logo /maserati.jpg" alt="logo maserati" width="100">
+    </div>
+    <div>
+      <img class="porsche" src="logo /porsche.webp" alt="logo porsche" height="100">
+    </div>
+    <div>
+      <img class="car-brand" src="logo /mercedes.png " alt="logo mercedes" >
+    </div>
+</div>
+</section>
+ 
