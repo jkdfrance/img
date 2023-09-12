6c94a863966d93f0771d46aac4e476e633ee6919
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 16:50:37 +0200

    Create article.php

---
+++ lib/article.php
@@ -0,0 +1,24 @@
+<?php 
+
+$articles = [ 
+    ["title" => "OPEL SUV", "content" => "Lodgy Stepway 1.5 dci 11 30 ch – 95180km... 17,125 €", "image" => "suv.jpg","define" => "Mise en circulation 26/11/2018 ","item" => "Kilométrage 44 016 km", "item1" => "Carburant Essence", "item2" => "Motorisation
+    1.2 Turbo 83 BVM5", "item3" => "Consommation 5,1 L/100km", "name" => "L'avis de Vincent PARROT", "definition" => "L'Opel suv est une expérience de conduite agréable grâce à son moteur puissant et efficace. Son design moderne et élégant attire tous les regards. L'intérieur spacieux et confortable permet d'accueillir toute la famille avec facilité. Les nombreuses fonctionnalités technologiques offrent une connectivité optimale pour une expérience de conduite plus intuitive. Avec le Crossland X Edition, chaque trajet devient une expérience unique.", "source0" => "opel.mp4"],
+    
+    ["title" => "TAXI-BROUSSE", "content" => "4x4 TOUT TERRAIN 4,000 000 €" , "image" => "taxi-brousse.jpg", "define" => "Mise en circulation 26/11/2023 ","item" => "Kilométrage 344 016 km", "item1" => "Carburant uranium", "item2" => "Motorisation
+    V8 4.5 PUR-SANG", "item3" => "Consommation 43.3 L/100km", "name" => "L'avis de Vincent PARROT", "definition" => "Peugeot  est une marque « bourgeoise », fidèle a ses solutions techniques mais aussi au cycle de vie de ses produits. Après la guerre, chaque berline devient la bourgeoise de la gamme,simple et pratique;mécanique fiable et éprouvée,procure un véritable sentiment de bien-être. Les fonctionnalités avancées et les équipements haut de gamme ajoutent une touche de luxe. Avec le Peugeot taxi-brousse << série limitée à 1 exemplaire >>, chaque trajet devient une expérience inoubliable,un argumentaire de vente imparable.", "source1" => "taxi-brousse.mp4"],
+    
+    ["title" => "CITROEN ", "content" => " Grand C4 Picasso 5,000", "image" => "citroen.webp",  "define" => "Mise en circulation 16/01/2023 ","item" => "Kilométrage 4 006 km", "item1" => " electrique", "item2" => "Motorisation
+    1.6 THP 155 BVM6", "item3" => "Consommation 22,3 l/100km", "name" => "L'avis de Vincent PARROT", "definition" => "Le Grand C4 Picasso, dans sa finition Exclusive, offre une expérience de conduite unique. Son moteur puissant garantit des performances exceptionnelles. L'intérieur spacieux et confortable procure un véritable sentiment de bien-être.", "source2" => "citroen-c4.mp4"],
+    
+    ["title" => "BUGATTI", "content" => "CHIRON 2,000 €", "image" => "bugatti.jpg", "define" => "Mise en circulation 25/01/2023 ","item" => "Kilométrage  106 km", "item1" => "kérosène", "item2" => "Motorisation
+    moteur W16", "item3" => "Consommation 6 L/100km", "name" => "L'avis de Vincent PARROT", "definition" => "La Chiron Sport « 110 ans Bugatti », annoncée en février 2028, est une série limitée de vingt exemplaires qui célèbre les 110 ans de Bugatti. La série spéciale « 110 ans Bugatti », dans une livrée bleu mat intitulée « Steel Blue.", "source3" => "bugatti.mp4"],
+    
+    ["title" => "DS-FANTOMAS", "content" => "DS PHANTOM 100,000 €", "image" => "ds-fantomas.jpg", "define" => "Mise en circulation 25/01/1963 ","item" => "Kilométrage  10 006 km", "item1" => "kérosène", "item2" => "Motorisation
+    moteur BI Turbo réacteur", "item3" => "Consommation 6 L/100km", "name" => "L'avis de Vincent PARROT", "definition" => "Le constructeur automobile français dévoile en effet un dessin inédit, qui est présenté comme “une adaptation moderne” de la DS volante du film “Fantômas se déchaîne” sorti en 1965. Ce visuel montre un concept car inédit de la marque, qui est doté de grosses jantes à rayons, d’une face avant pleine et bien sûr d’ailes proéminentes.", "source4" => "ds-volante.mp4"],
+    
+    ["title" => "MERCEDES", "content" => "2018 Vision Mercedes Maybach 6 Cabriolet ", "image" => "maybach.jpg", "define" => "Mise en circulation 15/04/2023 ","item" => "Kilométrage  10 006 km", "item1" => "éléctrique", "item2" => "Motorisation
+    éléctrique", "item3" => "Consommation 10 L/100km", "name" => "L'avis de Vincent PARROT", "definition" => "Ce concept-car est motorisé par quatre moteurs électriques (un pour chaque roue) d'une puissance totale de 738 ch (550 kW)9 (poussée à 750 ch pour le cabriolet), pour une vitesse de pointe de 250 km/h, et un 0 à 100 km/h en moins de quatre secondes. Une batterie lithium-ion de 80 kWh permet une autonomie annoncée de 500 km (un système de charge rapide permettrait de recharger 100 km d'autonomie en cinq minutes." ,"source5" => "maybach"],
+     
+  ];
+
+  ?>
