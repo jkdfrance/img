6fe2631449e109f68fa39b1143c62f31a9e1eb17
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 16:53:41 +0200

    Create menu.php

---
+++ lib/menu.php
@@ -0,0 +1,9 @@
+<?php 
+$mainMenu = [
+  "acceuil.php" => ["menu_title" => "acceuil", "head_title" => "menu acceuil", "meta_description"=> "tout sur le garage"],
+  "nos_voitures.php" =>["menu_title" => "nos vehicules", "head_title" => "acceuil menu", "meta_description" => "tous nos véhicule d'occasion","exclude" => false],
+  "entretien.php" =>[ "menu_title" => " entretien", "head_title" => "infos services","meta_description" => "services & entretien"],
+  "depannage.php" =>["menu_title" => "depannage", "head_title" => "infos depannage","meta_description" => "demandez un devis"],
+  "contact.php" =>[ "menu_title" => "contact", "head_title" => "infos contact", "meta_description" => "contactez nous"],
+];
+?>
