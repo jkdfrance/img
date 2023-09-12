b2109c14317a5573ef5362ff9c1ec75138170a01
site <119048961+jkdfrance@users.noreply.github.com>
2023-09-12 15:12:39 +0200

    Create header--2.php

---
+++ template/header--2.php
@@ -0,0 +1,57 @@
+<?php 
+$mainMenu = [
+  "acceuil.php" => ["menu_title" => "acceuil", "head_title" => "menu acceuil", "meta_description"=> "tout sur le garage"],
+  "nos_voitures.php" =>["menu_title" => "nos vehicules", "head_title" => "acceuil menu", "meta_description" => "tous nos véhicule d'occasion"],
+  "entretien.php" =>[ "menu_title" => " entretien", "head_title" => "infos services","meta_description" => "services & entretien"],
+];
+?>
+      <?php echo '<link rel="stylesheet" href="style.css">'; ?>
+      <?php $currentPage = basename($_SERVER["SCRIPT_NAME"]); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="déscription" content="<?=$mainMenu[$currentPage]["meta_description"] ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-3dZ9wIrMMij8rOH7X3kLfXAzwtcHpuYpEgQg1OA4QAob1e81H8ntUQmQm3pBudqIoySO5j0tHN4ENzA6+n2r4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css" />
+    <title><?=htmlentities($mainMenu[$currentPage]["head_title"]) ?></title>
+    <script src="stylesheet" href="https://kit.fontawesome.com/de8de52639.js"  crossorigin="anonymous" ></script>
+  </head>
+  <body>
+
+
+  <!--header-page-service.php-->
+
+  <header>
+  
+    <nav>
+  
+            <input type="checkbox" id="check">
+            <label for="check" class="btn">
+                <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Vincent Parrot</label>
+
+     
+    <ul class="list-acceuil">
+            <li><a href="acceuil.php">ACCEUIL</a></li>
+            <li><a href="nos_voitures.php">Nos voitures</a></li>
+            <li><a href="depannage.php">Depannage</a></li>
+            
+    </ul>
+  </nav>
+
+
+    <div class="imageCar">
+      <h1>REVISION</h1>
+      <p>Savez-vous de quand date la dernière révision de votre véhicule ? Connaissez-vous bien tous les risques d’une auto présentant un défaut d’entretien ? C’est le moment de réviser !</p>
+    </div>
+  </header>
+
+<main>
